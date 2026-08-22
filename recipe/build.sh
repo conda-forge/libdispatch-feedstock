58404aaf07be58e37ac03ed4343d9734a296da00
Chawye Hsu <su+git@chawyehsu.com>
2026-08-22 23:00:09 +0800

    build: remove -fno-plt on linux-ppc64le
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+# https://github.com/conda-forge/libdispatch-feedstock/pull/21#issuecomment-5374865695
+if [[ "$target_platform" == "linux-ppc64le" ]]; then
+  CFLAGS="$(echo $CFLAGS | sed 's/-fno-plt //g')"
+  CXXFLAGS="$(echo $CXXFLAGS | sed 's/-fno-plt //g')"
+fi
+
 cmake ${CMAKE_ARGS} \
     -G Ninja \
     -B build \
